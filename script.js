57aaa2f3bebbac9ec3c128bb5f1a1a8945e3478c
Alex93pa <Ale.pug2@gmail.com>
2021-04-22 16:28:04 +0200

    fine

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-var studenti = {
-    nome : "mario",
-    cognome : "rossi",
+//PRIMO 
+var studente = {
+    nome : "Mario",
+    cognome : "Rossi",
     eta : "27"
 }
 
-for (var key in studenti) {
-    console.log(key + ":", studenti[key]);
+for (var key in studente) {
+    console.log(key + ":", studente[key]);
 }
+
+//SECONDO
+var studenti = [
+    {
+        nome: "Romelu",
+        cognome: "Lukaku",
+        eta: 28
+    },
+    {
+        nome: "Lautaro",
+        cognome: "Martinez",
+        eta: 24
+    },
+    {
+        nome: "Nicolò",
+        cognome: "Barella",
+        eta: 23
+    }
+];
+
+for (var i = 0; i < studenti.length; i++){    
+    for(proprieta in studenti[i]) {
+        console.log(proprieta, studenti[i][proprieta]);
+    }  
+}
+
+//TERZO
+
+var studenteNuovo = {
+    nome: prompt("inserire un nome"),
+    cognome: prompt("inserire un cognome"),
+    eta: parseInt(prompt("inserire l'età"))
+}
+
+studenti.push(studenteNuovo);
+console.log(studenteNuovo)
+
+
